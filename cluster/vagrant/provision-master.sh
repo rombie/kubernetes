4e77f2f47fda7889d384bd789a3e00aca51e645c
Ananth Suryanarayana <anantharamu@gmail.com>
2015-04-07 15:21:36 -0700

    move code

--- cluster/vagrant/provision-master.sh
+++ cluster/vagrant/provision-master.sh
@@ -201,9 +201,6 @@ EOF
   # enabling the service (which is not an error) from being printed to stderr.
   SYSTEMD_LOG_LEVEL=notice systemctl enable salt-api
   systemctl start salt-api
-  if [ ! -z $NETWORKING_WITH_OPENCONTRAIL ]; then
-      curl -s https://raw.githubusercontent.com/rombie/opencontrail-netns/master/provision/fedora/contrail_install_controller | sh
-  fi
 fi
 
 if ! which salt-minion >/dev/null 2>&1; then
@@ -217,4 +214,8 @@ else
   echo "Executing configuration"
   salt '*' mine.update
   salt --show-timeout --force-color '*' state.highstate
+fi
+
+if [ ! -z $NETWORKING_WITH_OPENCONTRAIL ]; then
+    curl -s https://raw.githubusercontent.com/rombie/opencontrail-netns/master/provision/fedora/contrail_install_controller | sh
 fi
